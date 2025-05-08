34f566c775d2425fcd1ad28c1feb03c68288c082
freejo2003 <freejojaison555@gmail.com>
2025-05-08 20:00:39 +0530

    Added Odd-Even number checker

---
+++ 01_oddeven.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter a number:";
+    cin >> n;
+    if (n == 0)
+    {
+        cout << n << " is zero.";
+    }
+    else if (n % 2 == 0)
+    {
+        cout << n << " is even.";
+    }
+    else
+    {
+        cout << n << " is odd.";
+    }
+    return 0;
+}
